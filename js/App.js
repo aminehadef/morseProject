a11433fafa899155aa5491ba07ef9d5afb988040
amine <amine.hadef@epitech.eu>
2019-04-27 23:25:03 +0200

    simulation de bip bip

--- js/App.js
+++ js/App.js
@@ -1,7 +1,7 @@
 
 var inpText  = document.getElementById('text')
 var Ptranslation = document.getElementById('translation')
-var button = document.getElementById('button')
+var play = document.getElementById('play')
 var codeMorce = {
     "a" : {
         "value"  : "._",
@@ -187,11 +187,31 @@ var codeMorce = {
     }
 }
 inpText.addEventListener('keyup', (e)=>{
-    if(e.keyCode > 64 && e.keyCode < 91 || e.keyCode > 95 && e.keyCode < 106 || e.keyCode == 8 ){
+    if(e.keyCode > 64 && e.keyCode < 91 || e.keyCode > 95 && e.keyCode < 106 || e.keyCode == 8){
         Ptranslation.innerHTML = ''
         var expInpText = inpText.value.split('')
         for(let i = 0; i < expInpText.length; i++){
             Ptranslation.innerHTML += codeMorce[expInpText[i]].value
+        }
+    }
+    if (Ptranslation.innerHTML.length > 0) {
+        play.style.display = "block"
+    }
+    else{
+        play.style.display = "none"
+    }
+})
+
+play.addEventListener('click', ()=>{
+    var expMorseTxt = Ptranslation.innerHTML.split('')
+    for (let i = 0; i < expMorseTxt.length; i++) {
+        if (expMorseTxt[i] == ".") {
+            console.log('bip');
+        }else if(expMorseTxt[i] == "_"){
+            console.log('biiiiip');
+        }else if(expMorseTxt[i] == "/"){
+            console.log(' / ');
+            
         }
     }
 })
